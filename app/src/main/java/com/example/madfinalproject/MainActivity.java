4f76920fc99cce4387a10c1a2108d041b3d535b0
Sugandha <72730060+Sugandha-kadp@users.noreply.github.com>
2022-04-09 14:57:24 +0530

    09/04/2022 2.57 P.M.

--- app/src/main/java/com/example/madfinalproject/MainActivity.java
+++ app/src/main/java/com/example/madfinalproject/MainActivity.java
@@ -4,7 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+
+import java.sql.DatabaseMetaData;
+
 public class MainActivity extends AppCompatActivity {
+
+
+ DatabaseReference adddataRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
