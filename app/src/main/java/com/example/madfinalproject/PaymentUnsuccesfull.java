dda6f65f1f92f5436f8e2640991684a95b41a68b
Sugandha <72730060+Sugandha-kadp@users.noreply.github.com>
2022-05-06 02:59:39 +0530

    09/04/2022 2.57 P.M. Add Another activity and do some changes

---
+++ app/src/main/java/com/example/madfinalproject/PaymentUnsuccesfull.java
@@ -0,0 +1,14 @@
+package com.example.madfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentUnsuccesfull extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_unsuccesfull);
+    }
+}
--- app/src/main/java/com/example/madfinalproject/View_Card_Data.java
+++ app/src/main/java/com/example/madfinalproject/View_Card_Data.java
@@ -6,31 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.google.firebase.database.DatabaseReference;
-
-import java.sql.DatabaseMetaData;
-
-public class MainActivity extends AppCompatActivity {
-
-
- DatabaseReference adddataRef;
+public class View_Card_Data extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_view_card_data);
     }
-    //navigation
-    public void openViewCardData(View view){
-        Intent intent = new Intent(this,View_Card_Data.class);
-
-        startActivity(intent);
-    }
-
     //navigation Go Back
     public void goBack (View view){
         Intent intent = new Intent(this,MainActivity.class);
 
         startActivity(intent);
     }
+
 }
