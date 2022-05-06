97fd44739ac48a2d4fd4e1db85c293b216ef9514
Sugandha <72730060+Sugandha-kadp@users.noreply.github.com>
2022-05-07 02:57:34 +0530

    09/04/2022 2.57 P.M. Data Retrieve is done.

--- app/src/main/java/com/example/madfinalproject/ViewCardData.java
+++ app/src/main/java/com/example/madfinalproject/ViewCardData.java
@@ -1,16 +1,22 @@
 package com.example.madfinalproject;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.madfinalproject.databinding.ActivityViewCardDataBinding;
 import com.example.madfinalproject.databinding.ActivityViewCardDataBindingImpl;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.zip.DataFormatException;
 
@@ -33,7 +39,13 @@ public class ViewCardData extends AppCompatActivity {
         binding.btnGetData.setOnClickListener( new  View.OnClickListener(){
             @Override
             public void onClick(View v){
-//                String crdNumber = binding.
+                if(!newDate.isEmpty()){
+                    viewCrdData(newDate);
+                }
+                else {
+                    Toast.makeText(ViewCardData.this,"newDate notset",Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
@@ -42,8 +54,35 @@ public class ViewCardData extends AppCompatActivity {
         Intent intent =  getIntent();
         newDate = intent.getStringExtra("newDate");
 
-        textViewPaymentData.setText(newDate);
 
+    }
+
+    private void viewCrdData(String newDate){
+        reference = FirebaseDatabase.getInstance().getReference("Payment");
+        reference.child(newDate).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+
+                if(task.isSuccessful()){
+
+                   if(task.getResult().exists()){
+
+                       Toast.makeText(ViewCardData.this,"Successfully Read",Toast.LENGTH_SHORT).show();
+                        DataSnapshot dataSnapshot = task.getResult();
+                        String crdNumber = String.valueOf(dataSnapshot.child("crdNumber").getValue());
+                        binding.textViewCrdNumber.setText(crdNumber);
+                   }
+                   else {
+                       Toast.makeText(ViewCardData.this,"Payment does not exist",Toast.LENGTH_SHORT).show();
+                   }
+
+                }
+                else {
+                    Toast.makeText(ViewCardData.this,"Failed to read",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 
 
